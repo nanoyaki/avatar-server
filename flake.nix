e207aac65281b621abd095af27443563624f80f6
nanoyaki <hanakretzer@nanoyaki.space>
2026-03-21 21:50:14 +0100

    feat(nix): add overlays

--- flake.nix
+++ flake.nix
@@ -12,75 +12,102 @@
   outputs =
     inputs@{ flake-parts, ... }:
 
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, ... }:
 
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
+      {
+        systems = import inputs.systems;
 
-      perSystem =
-        { lib, pkgs, common, ... }:
+        imports = [
+          inputs.treefmt-nix.flakeModule
+        ];
 
-        {
-          _module.args.common = {
-            src = lib.cleanSource ./.;
+        flake.overlays = rec {
+          default =
+            _: prev:
 
-            nativeBuildInputs = with pkgs; [ pkg-config ];
-            buildInputs = with pkgs; [ openssl ];
-          };
+            withSystem prev.stdenv.hostPlatform.system (
+              { config, ... }:
 
-          packages.avatar-server = pkgs.callPackage (
-            { 
-              lib,
-              rustPlatform,
-            }:
+              {
+                inherit (config.packages) avatar-server;
+              }
+            );
 
-            let
-              cargoToml = lib.importTOML ./Cargo.toml;
-            in
+          avatar-server = default;
+        };
 
-            rustPlatform.buildRustPackage (common // {
-              pname = cargoToml.package.name;
-              inherit (cargoToml.package) version;
+        perSystem =
+          {
+            lib,
+            pkgs,
+            common,
+            ...
+          }:
 
-              cargoHash = "sha256-2XS97owOxQErQ+a0OSmZBx6XAXxWTzXKrHqgX/KRllw=";
+          {
+            _module.args.common = {
+              src = lib.cleanSource ./.;
 
-              meta = {
-                mainProgram = "avatar-server";
-                description = "Alternative avatar server implementation for tangled";
-                maintainers = with lib.maintainers; [ nanoyaki ];
-                license = lib.licenses.agpl3Plus;
-                # Maybe works on others too? Haven't tested it
-                platforms = lib.platforms.linux;
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+              buildInputs = with pkgs; [ openssl ];
+            };
+
+            packages.avatar-server = pkgs.callPackage (
+              {
+                lib,
+                rustPlatform,
+              }:
+
+              let
+                cargoToml = lib.importTOML ./Cargo.toml;
+              in
+
+              rustPlatform.buildRustPackage (
+                common
+                // {
+                  pname = cargoToml.package.name;
+                  inherit (cargoToml.package) version;
+
+                  cargoHash = "sha256-2XS97owOxQErQ+a0OSmZBx6XAXxWTzXKrHqgX/KRllw=";
+
+                  meta = {
+                    mainProgram = "avatar-server";
+                    description = "Alternative avatar server implementation for tangled";
+                    maintainers = with lib.maintainers; [ nanoyaki ];
+                    license = lib.licenses.agpl3Plus;
+                    # Maybe works on others too? Haven't tested it
+                    platforms = lib.platforms.linux;
+                  };
+                }
+              )
+            ) { };
+
+            devShells.default = pkgs.mkShell {
+              packages =
+                common.nativeBuildInputs
+                ++ common.buildInputs
+                ++ (with pkgs; [
+                  rustc
+                  cargo
+                  clippy
+                  rustfmt
+                  rust-analyzer-unwrapped
+                ]);
+
+              RUST_BACKTRACE = 1;
+              RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc.outPath;
+            };
+
+            treefmt = {
+              projectRootFile = "Cargo.toml";
+              programs = {
+                actionlint.enable = true;
+                nixfmt.enable = true;
+                rustfmt.enable = true;
               };
-            })
-          ) { };
-
-          devShells.default = pkgs.mkShell {
-            packages =
-              common.nativeBuildInputs
-              ++ common.buildInputs
-              ++ (with pkgs; [
-                rustc
-                cargo
-                clippy
-                rustfmt
-                rust-analyzer-unwrapped
-              ]);
-
-            RUST_BACKTRACE = 1;
-            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc.outPath;
-          };
-
-          treefmt = {
-            projectRootFile = "Cargo.toml";
-            programs = {
-              actionlint.enable = true;
-              nixfmt.enable = true;
-              rustfmt.enable = true;
             };
           };
-        };
-    };
+      }
+    );
 }
